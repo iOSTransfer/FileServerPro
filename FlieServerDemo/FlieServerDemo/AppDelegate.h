8a62671b14578503361d216eb12b313aa3811977
lyric <lyric@lyricdeMac-mini.local>
2017-04-20 17:24:39 +0800

    简易实现socket服务端和客户端数据的传输

---
+++ FlieServerDemo/FlieServerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlieServerDemo
+//
+//  Created by lyric on 2017/4/20.
+//  Copyright © 2017年 lyric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlieServerDemo/FlieServerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlieServerDemo
+//
+//  Created by lyric on 2017/4/20.
+//  Copyright © 2017年 lyric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
